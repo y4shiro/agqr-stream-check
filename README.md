d81e47d8410bc822e9fe7dce97308c3a80947293
y4shiro <y4shironao@gmail.com>
2021-04-05 18:47:59 +0900

    Slack への通知を実装

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # agqr-stream-check
 
-超 A&G の配信 URL が生きているかチェック
+超 A&G の配信 URL が生きているかチェック  
+レスポンスが 200 以外を返した場合は Slack に通知する
 
 ## 環境
 
-- ruby
+- Ruby 2.7.1 で動作確認
+- dotenv
 
 ## 使い方
+
+Ruby の Gem である dotenv を使用するので、`bundle install` を事前に行う
+
+```
+$ bundle install
+```
+
+`.env` ファイルを同じディレクトリに設置し、Slack の Webhook URL を記述する
+
+```
+URL=<SLACK_WEBHOOK_URL>
+```
 
 配信 URL にリクエスト飛ばして 200 か否かチェックするだけ  
 Crontab などで一日一回程度実行する
--- check.rb
+++ check.rb
@@ -1,7 +1,12 @@
 require 'open-uri'
+require 'json'
+require 'dotenv'
 
 STREAM_URL = 'https://www.uniqueradio.jp/agplayer5/hls/mbr-ff.m3u8'
 BAD_URL = 'https://example.com/a'
+
+Dotenv.load
+p ENV['URL']
 
 begin
   uri = URI.parse(STREAM_URL)
@@ -19,5 +24,11 @@ begin
   p bad_res.read
 rescue => e
   p e
+
+  data = { 'text' => "配信 URL のレスポンスが #{e} となっています。確認してください。" }
+  reqest_url = ENV['URL']
+  uri = URI.parse(reqest_url)
+  Net::HTTP.post_form(uri, {"payload" => data.to_json})
 end
+
 
